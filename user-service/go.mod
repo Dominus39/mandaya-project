6721e7aca89fc3722dd6f708bd4bf221df891b49
Yugo Alexander <yugoalexander@gmail.com>
2025-02-05 06:26:10 +0700

    feat: user service login function

--- user-service/go.mod
+++ user-service/go.mod
@@ -18,6 +18,7 @@ require (
 )
 
 require (
+	github.com/golang-jwt/jwt/v4 v4.5.1
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.5.5 // indirect
---
+++ user-service/internal/handler/login.go
@@ -0,0 +1,54 @@
+package handler
+
+import (
+	"net/http"
+	"time"
+	"user-service/config"
+	"user-service/dto"
+	"user-service/models"
+
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/labstack/echo/v4"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// LoginUser godoc
+// @Summary Login a user
+// @Description This endpoint allows users to login by providing email and password.
+// @Tags Users
+// @Accept json
+// @Produce json
+// @Param login body entity.LoginRequest true "Login User"
+// @Success 200 {object} entity.LoginResponse
+// @Failure 400 {object} map[string]interface{} "Invalid Request Parameters"
+// @Failure 401 {object} map[string]interface{} "Invalid Email"
+// @Failure 401 {object} map[string]interface{} "Invalid Password"
+// @Failure 500 {object} map[string]interface{} "Invalid Generate Token"
+// @Router /users/login [post]
+func LoginUser(c echo.Context) error {
+	var req dto.LoginRequest
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"message": "Invalid Request Parameters"})
+	}
+	var user models.User
+	err := config.DB.Where("email = ?", req.Email).First(&user).Error
+	if err != nil {
+		return c.JSON(http.StatusUnauthorized, map[string]string{"message": "Invalid Email"})
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(req.Password)); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"message": "Invalid Password"})
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id":  user.ID,
+		"exp": time.Now().Add(time.Hour * 72).Unix(),
+	})
+
+	tokenString, err := token.SignedString([]byte("JWT_SECRET"))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": "Invalid Generate Token"})
+	}
+
+	return c.JSON(http.StatusOK, dto.LoginResponse{Token: tokenString})
+}
