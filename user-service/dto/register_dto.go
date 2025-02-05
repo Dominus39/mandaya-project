45221c0eda5e1d0e8a7514a43bc0e16d946dbfb6
Yugo Alexander <yugoalexander@gmail.com>
2025-02-05 12:44:42 +0700

    fix: change config and add routes to main

--- user-service/dto/register_dto.go
+++ user-service/dto/register_dto.go
@@ -1,6 +1,6 @@
 package dto
 
-type RegisterUser struct {
+type RegisterRequest struct {
 	Name     string `json:"name" validate:"required"`
 	Email    string `json:"email" validate:"required, email"`
 	Password string `json:"password" validate:"required"`
